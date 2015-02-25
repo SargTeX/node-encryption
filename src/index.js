605d3362e0097606822b5de73f1027886b3d2ff8
SargTeX <martin.christian.bories@gmail.com>
2015-02-25 20:04:23 +0100

    Corrected package name

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ module.exports.config = function(name, value) {
 	config[name] = value;
 }
 
-module.exports.oneWay = function(model, fieldName, options) {
+module.exports.oneWay = function(schema, fieldName, options) {
 	if (!options) options = {};
 	if (!options.hasOwnProperty('compareMethod')) options.compareMethod = false;
 	if (!options.hasOwnProperty('compareMethodName')) options.compareMethodName = 'compare'+fieldName.charAt(0).toUpperCase()+fieldName.substring(1);
 
-	model.pre('save', function(callback) {
+	schema.pre('save', function(callback) {
 		var object = this;
 
 		// verschlüssele das Passwort nur, wenn es geändert wurde
@@ -33,7 +33,7 @@ module.exports.oneWay = function(model, fieldName, options) {
 			});
 		});
 	});
-	if (options.compareMethod) model.methods[options.compareMethodName] = function(candidate, callback) {
+	if (options.compareMethod) schema.methods[options.compareMethodName] = function(candidate, callback) {
 		bcrypt.compare(candidate, this[fieldName], function(err, isMatch) {
 			if (err) return callback(err);
 			callback(null, isMatch);
